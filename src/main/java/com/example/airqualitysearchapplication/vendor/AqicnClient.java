934f73d9857e13ee7adb02466f28340a8c924f27
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-25 14:30:55 +0530

    THis is a vendor

--- src/main/java/com/example/airqualitysearchapplication/vendor/AqicnClient.java
+++ src/main/java/com/example/airqualitysearchapplication/vendor/AqicnClient.java
@@ -5,7 +5,9 @@ import com.example.airqualitysearchapplication.dto.CitySearchResponseDto;
 import com.example.airqualitysearchapplication.model.AirQualityData;
 import com.github.benmanes.caffeine.cache.Cache;
 import lombok.RequiredArgsConstructor;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
