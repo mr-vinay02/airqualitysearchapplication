1b3c7e51703b55e8843bd4c67afee70735e6ad82
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-25 15:09:01 +0530

    This is a exception

---
+++ src/main/java/com/example/airqualitysearchapplication/exception/CityNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.airqualitysearchapplication.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CityNotFoundException extends ApiException {
+
+    public CityNotFoundException(String city) {
+        super("City not found: " + city, HttpStatus.NOT_FOUND);
+    }
+}
