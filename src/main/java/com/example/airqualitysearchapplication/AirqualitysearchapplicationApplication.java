877ed08a1204abb07e70307ed3627586f6710a29
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-25 12:00:42 +0530

    Directory structure

---
+++ src/main/java/com/example/airqualitysearchapplication/AirqualitysearchapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.airqualitysearchapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirqualitysearchapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirqualitysearchapplicationApplication.class, args);
+	}
+
+}
